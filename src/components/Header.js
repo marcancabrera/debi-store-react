fcecebe8e34bda6bce859ab19b00067c9925bc10
markandeia <marcandeacabrera@gmail.com>
2021-06-01 14:52:49 -0400

    Add init structure

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return <div className="">
+      <h1>Ojalateria Debi</h1>
+    </div>;
+  }
+}
+
+export default Header;
