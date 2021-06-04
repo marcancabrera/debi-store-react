5bd29ac64af67ab7f232565f053dd589383fb640
markandeia <marcandeacabrera@gmail.com>
2021-06-03 22:59:26 -0400

    Delete files to create in new places

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import AppContent from './content/app-content';
+
+const ROUTES = {
+  home: '/'
+};
+
+class App extends Component {
+  render () {
+		const APP_CONTENT = AppContent;
+
+		return (
+			<Router>
+				<Switch>
+					<Route path={ROUTES.home} component={Home}>
+						<Home content={APP_CONTENT} />
+					</Route>
+				</Switch>
+			</Router>
+		);
+	}
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Header from './components/Header';
+import App from "./App";
 
-const container = document.getElementById('header');
-
-ReactDOM.render(<Header />, container);
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
