17c776024925ef3d8c1ecd3a4f0d36d4d5bcfaa5
markandeia <marcandeacabrera@gmail.com>
2021-06-04 11:01:12 -0400

    Create cover Page

---
+++ src/modules/CoverPage/CoverPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './CoverPage.scss'
+
+function CoverPage(props) {
+  const content = props.content;
+  const scssClass = 'CoverPage';
+
+  return (
+    <div className={scssClass}>
+    </div>
+  );
+}
+
+export default CoverPage;
