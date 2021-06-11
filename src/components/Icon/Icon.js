2accee5f62ce00b793be9a612811deb9cbfdfba7
markandeia <marcandeacabrera@gmail.com>
2021-06-11 01:32:54 -0400

    Remove and add icons

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,14 +1,11 @@
 import React from "react";
 
 import facebook from "../../images/facebook.svg";
-import fries from "../../images/fries.svg";
-import hamburger from "../../images/hamburger.svg";
-import iceCream from "../../images/ice-cream.svg";
 import instagram from "../../images/instagram.svg";
-import lightHamburger from "../../images/light-hamburger.svg";
 import nav from "../../images/nav.svg";
-import soda from "../../images/soda.svg";
-import tiktok from "../../images/tiktok.svg";
+import search from "../../images/search.svg";
+import shoppingcart from "../../images/shoppingcart.svg";
+import debi from "../../images/debi.svg";
 
 import "./Icon.scss";
 
@@ -17,13 +14,10 @@ function Icon(props) {
   const icons = {
     'instagram': instagram,
     'facebook': facebook,
-    'tiktok': tiktok,
-    'hamburger': hamburger,
-    'lightHamburger': lightHamburger,
-    'soda': soda,
-    'fries': fries,
-    'iceCream': iceCream,
     'nav': nav,
+    'search': search,
+    'shoppingcart': shoppingcart,
+    'debi': debi,
   };
 
   return (
