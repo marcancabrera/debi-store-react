c622bd87c71cc80d165887854088622d02ee43ab
markandeia <marcandeacabrera@gmail.com>
2021-06-12 00:44:39 -0400

    Create files

---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Tab.scss";
+
+function Tabs(props) {
+  const scssClass = "Tabs";
+
+  const TabList = () => {};
+  return <p>Hola</p>;
+}
+
+export default Tabs;
