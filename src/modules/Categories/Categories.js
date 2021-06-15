7891746c6ec806cc0cac1ce2dcf647cb26edb11f
markandeia <marcandeacabrera@gmail.com>
2021-06-14 21:21:05 -0400

    Remove unnecessary lines

--- src/modules/Categories/Categories.js
+++ src/modules/Categories/Categories.js
@@ -4,7 +4,6 @@ import Tabs from '../../components/Tabs/Tabs';
 
 function Categories (props){
     const content = props.content;
-    const scssClass = "Categories";
 
     return (
         <Tabs tabs={content.tabs} icons={content.icons}/>
