fed43d4d1ac849b33762fec60104785eb209232a
markandeia <marcandeacabrera@gmail.com>
2021-07-01 15:49:08 -0400

    Add style to button

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-import image from "../../images/profile.png"
+import image from "../../images/profile.png";
+import Icon from "../Icon/Icon";
+
 import "./Detail.scss";
 
 function Detail(props) {
   const content = props.content;
   const scssClass = "Detail";
+  const icon = "shoppingcartwhite";
 
   const Info = () => {
     return (
@@ -13,18 +16,19 @@ function Detail(props) {
         <h3>{content.name}</h3>
         <h5>{content.description}</h5>
         <hr />
-        <h3>{content.price}</h3>
+        <p className="price">{content.price}</p>
       </div>
     );
   };
 
   const Buttons = () => {
     return (
-      <div className={`${scssClass}-buttons`}>
+      <div className={`${scssClass}-container-buttons`}>
         <button>1</button>
-        <button>Agregar producto</button>
-        <hr />
-        <button>Zonas y costo por delivery</button>
+        <button className={`${scssClass}-button-add`}>
+          <Icon type={"secondary"} icon={icon} />
+          <h5>AGREGAR</h5>
+        </button>
       </div>
     );
   };
@@ -47,9 +51,11 @@ function Detail(props) {
   };
 
   return (
-    <div className="grid-detail">
-      {RightSide()}
-      {LeftSide()}
+    <div className={`${scssClass}`}>
+      <div className="grid-detail">
+        {RightSide()}
+        {LeftSide()}
+      </div>
     </div>
   );
 }
